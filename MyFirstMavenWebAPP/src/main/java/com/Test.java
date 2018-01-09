7852929df5a03ac9f31a057c535354e3ae608508
gitForXuming <xuming_1024@163.com>
2018-01-09 18:20:52 +0800

    submit

--- MyFirstMavenWebAPP/src/main/java/com/Test.java
+++ MyFirstMavenWebAPP/src/main/java/com/Test.java
@@ -15,6 +15,7 @@ import java.util.Set;
 public class Test {
 	public volatile boolean flag =true;
 	public static void main(String[] args) {
+		//version 2
 		String hc = "ab";
 		System.out.println(hc.hashCode());
 		Set set = new HashSet();
