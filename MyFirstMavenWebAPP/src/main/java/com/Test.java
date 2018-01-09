8298976d6d49dfe1ceb3c4aeddb756bd423b0619
gitForXuming <xuming_1024@163.com>
2018-01-09 11:02:14 +0000

    Update Test.java
    version6

--- MyFirstMavenWebAPP/src/main/java/com/Test.java
+++ MyFirstMavenWebAPP/src/main/java/com/Test.java
@@ -15,7 +15,7 @@ import java.util.Set;
 public class Test {
 	public volatile boolean flag =true;
 	public static void main(String[] args) {
-		//version 5
+		//version 6
 		String hc = "ab";
 		System.out.println(hc.hashCode());
 		Set set = new HashSet();
